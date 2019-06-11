385fc3b36e58a39700e54761cc09818917212235
root <Alejandrra>
2019-06-10 23:22:31 -0500

    first commit

---
+++ cache/compiled/files/15502ca7af06c6ae2131f69be0c30f10.yaml.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/var/www/html/grav/user/accounts/admin.yaml',
+    'modified' => 1560215073,
+    'data' => [
+        'email' => 'nicole73711@gmail.com',
+        'fullname' => 'Alejandra Montenegro',
+        'title' => 'Administrador',
+        'state' => 'enabled',
+        'access' => [
+            'admin' => [
+                'login' => true,
+                'super' => true
+            ],
+            'site' => [
+                'login' => true
+            ]
+        ],
+        'hashed_password' => '$2y$10$Ps.Po4y0Ic429jw50AfoIukZ7GjthxLQPJ4M325n6ntHnAufelZue'
+    ]
+];
---
+++ cache/compiled/files/52005b2c6b9a9fd2a3a6f39d633bbb55.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/var/www/html/grav/user/config/security.yaml',
+    'modified' => 1560214981,
+    'data' => [
+        'salt' => 'tY3keCdNQTB5gX'
+    ]
+];
